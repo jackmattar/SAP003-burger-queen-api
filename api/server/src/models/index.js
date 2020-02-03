2314605f8627e62c6559819e86554c17fd180ee5
Jack Mattar <jackeline_mattar@hotmail.com>
2020-02-03 01:54:52 -0300

    configurações para deploy heroku

--- api/server/src/models/index.js
+++ api/server/src/models/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '../config/config.js')[env];
+const config = require('../config/config.js')[env];
 const db = {};
 
 let sequelize
@@ -33,7 +33,6 @@ Object.keys(db).forEach((modelName) => {
   }
 })
 
-'banana'
 db.sequelize = sequelize
 db.Sequelize = Sequelize
 
